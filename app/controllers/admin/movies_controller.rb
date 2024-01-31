cfa2a903d939432ef3c74dd1034d604018ddedb7
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2024-02-01 07:21:30 +0900

    管理者画面の作成

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -13,12 +13,9 @@ class Admin::MoviesController < ApplicationController
         if @movie.save
             redirect_to admin_movies_path, notice: "Movie was successfully created.", status: :found
         else
-            flash.now[:alert] = @movie.errors.full_messages.to_sentence
-            render :new, status: :unprocessable_entity
+            @movie.errors.add(:base, @movie.errors.full_messages)
+            render :new, status: :bad_request
         end
-    rescue ActiveRecord::RecordNotUnique
-        flash.now[:notice] = "Movie name is already taken"
-        render :new, status: :bad_request
     end
 
     def movie_params 
--- db/migrate/20190105082551_create_movie.rb
+++ db/migrate/20190105082551_create_movie.rb
@@ -1,7 +1,7 @@
 class CreateMovie < ActiveRecord::Migration[6.1]
   def change
     create_table :movies do |t|
-      t.string "name", limit: 160, null: false ,comment: "映画のタイトル。邦題・洋題は一旦考えなくてOK"
+      t.string "name", limit: 160, null: false, comment: "映画のタイトル。邦題・洋題は一旦考えなくてOK"
       t.string "year", limit: 45, comment: "公開年"
       t.text "description", comment: "映画の説明文"
       t.string "image_url", limit: 150, comment: "映画のポスター画像が格納されているURL"
