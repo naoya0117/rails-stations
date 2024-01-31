7096650ec5cb4950a662b3424037b24f548c77dd
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2024-02-01 04:34:26 +0900

    管理者画面の作成

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -2,4 +2,27 @@ class Admin::MoviesController < ApplicationController
     def index
         @movies = Movie.all
     end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.new(movie_params)
+
+        if @movie.save
+            redirect_to admin_movies_path, notice: "Movie was successfully created.", status: :found
+        else
+            flash.now[:alert] = @movie.errors.full_messages.to_sentence
+            render :new, status: :unprocessable_entity
+        end
+    rescue ActiveRecord::RecordNotUnique
+        flash.now[:notice] = "Movie name is already taken"
+        render :new, status: :bad_request
+    end
+
+    def movie_params 
+        params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
+    end
+
 end
--- app/views/movies/_movie.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! movie, :id, :name, :year, :description, :image_url, :is_showing, :created_at, :updated_at
-json.url movie_url(movie, format: :json)
