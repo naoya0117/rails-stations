750374b0f24ad19c352793e036461e67ddc5a332
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2024-02-04 13:41:04 +0900

    座席表の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
     root to: redirect('/admin/movies')
     resources :movies, only: [:index, :new, :create, :edit, :update, :destroy]
   end
+
   root to: redirect('/movies')
 
   resources :movies, only: [:index]
+  resources :sheets, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
