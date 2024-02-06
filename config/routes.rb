9e3ed29233da60a7ff1e651f54fb8739ed50a589
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2024-02-06 21:42:53 +0900

    上映予定時間の編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   namespace :admin do
     root to: redirect('/admin/movies')
-    resources :movies, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :movies, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+      resources :schedules, only: [:new, :show, :create, :edit, :destroy]
+    end
+    resources :schedules, only: [:index, :show, :update, :destroy]
   end
 
   root to: redirect('/movies')
 
   resources :movies, only: [:index, :show]
-  resources :sheets, only: [:index]
+  resource :sheets, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
