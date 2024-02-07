725e7ca5910714929f57127c960c281802f84755
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2024-02-07 18:29:29 +0900

    管理者画面-予約編集機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :admin do
     root to: redirect('/admin/movies')
     resources :movies, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
