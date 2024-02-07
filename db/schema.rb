9f130f7aca3420d20971d6880372b4bdadc0271e
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2024-02-07 18:39:00 +0900

    管理者画面-予約編集機能の追加

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 2024_02_07_091746) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name"
+    t.string "name", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
