d44bf7c9612f3ab0a554092c0cd217fc20000796
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2024-02-07 19:07:34 +0900

    管理者画面-予約編集機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   validates :password, presence: true
   validates :password_confirmation, presence: true
 
+  EMAIL_PATTERN = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: EMAIL_PATTERN }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
